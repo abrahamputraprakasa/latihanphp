0df6b680510ad7767910cac7bfcc96ed581ba762
Abraham Putra P <abrahamprakasa@gmail.com>
2023-07-19 20:24:41 +0700

    feat: php mysql

--- index.php
+++ index.php
@@ -14,6 +14,45 @@
 
 <body>
     <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "latihan";
+    $port = "33061";
+
+    //MySQLi
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database, $port);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected to MySQL successfully using MySQLi <br>";
+
+    $sql = "SELECT id, `name`, email FROM users";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"] . " - Name: " . $row["name"] . " - Email: " . $row["email"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+
+    //PDO
+    // try {
+    //     $conn = new PDO("mysql:host=$servername;dbname=$database;port=$port", $username, $password);
+    //     // set the PDO error mode to exception
+    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     echo "Connected to MySQL successfully using PDO";
+    // } catch (PDOException $e) {
+    //     echo "Connection failed: " . $e->getMessage();
+    // }
+
     $sekarang = date('Y-m-d H:i:s');
     $user = '';
     if (isset($_GET['user'])) {
--- submit.php
+++ submit.php
@@ -1,7 +1,41 @@
-<?php 
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "latihan";
+    $port = "33061";
+
+    //MySQLi
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database, $port);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
     $user = $_POST['user'];
     $email = $_POST['email'];
 
-    echo 'User: '. $user;
-    echo ', Email: '. $email;
+
+    // //Insert Biasa
+    // $sql = "INSERT INTO users (email, `name`)
+    // VALUES ('$user', '$email')";
+
+    // if ($conn->query($sql) === TRUE) {
+    //     echo "$user created successfully";
+    // } else {
+    //     echo "Error: " . $sql . "<br>" . $conn->error;
+    // }
+
+    // Prepared Statement
+    $stmt = $conn->prepare("INSERT INTO users (email, `name`) VALUES (?, ?)");
+    $stmt->bind_param("ss", $email, $user);
+    $stmt->execute();
+    echo "$user created successfully";
+
+    $conn->close();
+
+    echo "<a href='/latihan'>Back to Home</a>"
 ?>
