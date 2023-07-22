2e22047555015d8a6e1931aa23324323d5c9b963
Abraham Putra P <abrahamprakasa@gmail.com>
2023-07-22 22:02:29 +0700

    feat: datatables

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <html>
 
 <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css" />
     <style>
         .atas {
             background-color: #eee;
@@ -10,7 +13,6 @@
             background-color: #ccc;
         }
     </style>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 </head>
 
 <body>
@@ -48,7 +50,15 @@
             <input type="text" name="email" placeholder="Email" autocomplete="off"></input>
             <button>Submit</button>
         </form>
-
+        <div>
+            <table id="tabelku">
+                <thead>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </thead>
+            </table>
+        </div>
         <br>
         <button id="hitme">Hit me</button>
         <button onclick="getUsers()">Get Users</button>
@@ -69,11 +79,34 @@
         function getUsers() {
             $.ajax({
                 url: "/latihan/getusers.php",
-                data: {
-                },
+                data: {},
+                dataType: 'json',
                 success: function(result) {
                     console.log(result);
-                    // $("#weather-temp").html("<strong>" + result + "</strong> degrees");
+                    // let tabelku = "<table><thead><th>ID</th><th>Name</th><th>Email</th></thead>";
+                    // for (let index = 0; index < result.length; index++) {
+                    //     const element = result[index];
+                    //     tabelku += "<tbody><tr>";
+                    //     tabelku += "<td>" + element.id + "</td>";
+                    //     tabelku += "<td>" + element.name + "</td>";
+                    //     tabelku += "<td>" + element.email + "</td>";
+                    //     tabelku += "</tbody></tr>";
+                    // }
+                    // $("#tabelku").html(tabelku);
+
+                    $('#tabelku').DataTable({
+                        data: result,
+                        columns: [{
+                                data: 'id'
+                            },
+                            {
+                                data: 'name'
+                            },
+                            {
+                                data: 'email'
+                            },
+                        ]
+                    });
                 }
             });
         }
